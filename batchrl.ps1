f8cfad0e2cf4633a4557a48ff69d9110538f960f
j-mld <julien.mollard@protonmail.com>
2022-12-02 22:32:33 +0100

    Added modifications to the batch script

--- batchrl.ps1
+++ batchrl.ps1
@@ -1,9 +1,14 @@
 param([Int32]$num_processes=12)
+$data = @(10000, 1000, 100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001)
+
 
 for($i = 0; $i -lt $num_processes; $i++)
 {
-    start powershell {python run_sb3.py ; Read-Host}
-    #invoke-expression 'cmd /c start powershell {echo python run_sb3.py; Read-Host}'
+    $argument = $data[$i]
+
+    Start-Process powershell -ArgumentList "python run_sb3.py $argument"
+    #invoke-expression 'cmd /c start powershell {echo "python run_sb3.py"; Read-Host}'
+    Write-Host "Learning Rate of: " $data[$i]
 }
 
 Write-Host "Started $i processes"
